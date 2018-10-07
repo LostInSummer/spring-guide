8a3b2cbf33764bfa71d3ed7bbf6ff11159d0c8ee
chenjianlong <757232800@qq.com>
2018-10-07 16:33:36 +0800

    spring-authenticating-ldap提交

---
+++ spring-authenticating-ldap/src/main/java/com/lostinsummer/spring/authenticating/ldap/SpringAuthenticatingLdapApplication.java
@@ -0,0 +1,13 @@
+package com.lostinsummer.spring.authenticating.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticatingLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticatingLdapApplication.class, args);
+	}
+
+}
