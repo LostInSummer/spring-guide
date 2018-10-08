f127c3d1261bc77b7df261675b7c123fbf849324
chenjianlong <757232800@qq.com>
2018-10-08 09:39:37 +0800

    spring-actuator-service提交

---
+++ spring-actuator-service/src/main/java/com/lostinsummer/spring/actuator/service/SpringActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lostinsummer.spring.actuator.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorServiceApplication.class, args);
+	}
+
+}
