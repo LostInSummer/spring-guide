7809696b35bce177bd9f67e64ab9fd6a4e6a359b
chenjianlong <757232800@qq.com>
2018-10-08 10:58:20 +0800

    spring-messaging-jms提交 补充

---
+++ spring-messaging-jms/src/main/java/com/lostinsummer/spring/messaging/jms/Receiver.java
@@ -0,0 +1,20 @@
+package com.lostinsummer.spring.messaging.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+	// , containerFactory = "myFactory"
+	@JmsListener(destination = "mailbox")
+	public void receiveMessage(Email email) {
+		System.out.println("Received <" + email + ">");
+	}
+
+	@JmsListener(destination = "text-jms")
+	public void testMessage(Email email) {
+		System.out.println("Received <" + email + ">");
+	}
+
+}
