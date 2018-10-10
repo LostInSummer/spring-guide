ea28c12fc6a5d68458526f70c9e00ad8f9d3e0ea
chenjianlong <757232800@qq.com>
2018-10-10 11:22:51 +0800

    spring-rest-hateoas提交

---
+++ spring-rest-hateoas/src/main/java/com/lostinsummer/spring/rest/hateoas/Greeting.java
@@ -0,0 +1,26 @@
+package com.lostinsummer.spring.rest.hateoas;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting extends ResourceSupport {
+
+	private final String content;
+
+	@JsonCreator
+	public Greeting(@JsonProperty("content") String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting [content=" + content + ", getLinks()=" + getLinks() + "]";
+	}
+
+}
---
+++ spring-rest-hateoas/src/main/java/com/lostinsummer/spring/rest/hateoas/SpringRestHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.lostinsummer.spring.rest.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestHateoasApplication.class, args);
+	}
+
+}
