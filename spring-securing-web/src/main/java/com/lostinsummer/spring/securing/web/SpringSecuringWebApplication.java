959f8e54b50ea82951a9005d2019fa38761f1261
chenjianlong <757232800@qq.com>
2018-10-10 10:13:51 +0800

    spring-securing-web提交

---
+++ spring-securing-web/src/main/java/com/lostinsummer/spring/securing/web/SpringSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.lostinsummer.spring.securing.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuringWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SpringSecuringWebApplication.class, args);
+	}
+
+}
